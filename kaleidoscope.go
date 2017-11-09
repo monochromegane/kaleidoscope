9c1886d69f5a37f8ddc34b59c5b25af0ab36612a
monochromegane <dev.kuro.obi@gmail.com>
2017-11-09 10:36:41 +0900

    Added kaleidoscope struct.

--- kaleidoscope.go
+++ kaleidoscope.go
@@ -1,1 +1,16 @@
 package kaleidoscope
+
+type Kaleidoscope struct {
+	client Client
+}
+
+func New() (Kaleidoscope, error) {
+	client, err := NewClient()
+	if err != nil {
+		return Kaleidoscope{}, err
+	}
+
+	return Kaleidoscope{
+		client: client,
+	}, nil
+}
