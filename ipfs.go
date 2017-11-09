33bf2d55f448be7851dd63d002abb82382cb5b33
monochromegane <dev.kuro.obi@gmail.com>
2017-11-09 10:24:19 +0900

    Implemented IPFS struct and initializers.

---
+++ ipfs.go
@@ -0,0 +1,71 @@
+package kaleidoscope
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+
+	homedir "github.com/mitchellh/go-homedir"
+	ma "github.com/multiformats/go-multiaddr"
+	manet "github.com/multiformats/go-multiaddr-net"
+)
+
+const (
+	DefaultPathName   = ".ipfs"
+	DefaultPathRoot   = "~/" + DefaultPathName
+	DefaultApiFile    = "api"
+	DefaultKeystore   = DefaultPathRoot + "/keystore"
+	EnvDir            = "IPFS_PATH"
+	EmptyDirMultiHash = "QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn"
+)
+
+type IPFS struct {
+	url    string
+	client http.Client
+}
+
+func NewLocalIPFS() (IPFS, error) {
+	baseDir := os.Getenv(EnvDir)
+	if baseDir == "" {
+		baseDir = DefaultPathRoot
+	}
+
+	baseDir, err := homedir.Expand(baseDir)
+	if err != nil {
+		return IPFS{}, err
+	}
+
+	apiFile := path.Join(baseDir, DefaultApiFile)
+
+	if _, err := os.Stat(apiFile); err != nil {
+		return IPFS{}, err
+	}
+
+	api, err := ioutil.ReadFile(apiFile)
+	if err != nil {
+		return IPFS{}, err
+	}
+
+	return NewIPFS(strings.TrimSpace(string(api)))
+}
+
+func NewIPFS(url string) (IPFS, error) {
+	if a, err := ma.NewMultiaddr(url); err == nil {
+		_, host, err := manet.DialArgs(a)
+		if err == nil {
+			url = host
+		}
+	}
+	c := http.Client{
+		Transport: &http.Transport{
+			DisableKeepAlives: true,
+		},
+	}
+
+	return IPFS{
+		url:    url,
+		client: c,
+	}, nil
+}
